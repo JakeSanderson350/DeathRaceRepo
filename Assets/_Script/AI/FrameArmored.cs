86488f430effe0724b394934b3ed013ad3c50e2e
Isabel Rowland <isabel.rowland@mymail.champlain.edu>
2025-09-16 18:14:30 -0400

    Set up Car controller more extensively and finished implementing damage calculations for all frames

--- Assets/_Script/AI/FrameArmored.cs
+++ Assets/_Script/AI/FrameArmored.cs
@@ -6,33 +6,22 @@ using UnityEngine.Rendering.UI;
 
 public class FrameArmored : Frame
 {
-    public FrameStats stats;
     public override void HandleImpact(Frame other)
     {
         Vector3 vec1 = GetComponentInParent<Rigidbody>().velocity;
         Vector3 vec2 = other.GetComponentInParent<Rigidbody>().velocity;
         float dot = Vector3.Dot(vec1.normalized, vec2.normalized);
-        dot *= -1;
-        dot += 1;
-        dot /= 2;
+        dot *= -1; // reverse values so that a more negative dot product results in a higher damage value
+        dot += 1; // make all possible dot values positive
+        dot /= 2; // clamp from 0 to 1
         Frame frame2;
         if (other.GetType() == typeof(FrameArmored))
         {
             frame2 = other as FrameArmored;
+            float damageToThis = ImpactController.Instance.ArmoredDamageBase * dot * ImpactController.Instance.DamageMultiplier * vec2.magnitude;
             
+            // Damage Health, Damage popularity
             
         }
-        else if (other.GetType() == typeof(FrameRegular))
-        {
-            frame2 = other as FrameRegular;
-        }
-        else if (other.GetType() == typeof(FrameVulnerable))
-        {
-            frame2 = other as FrameVulnerable;
-        }
-
-
-
-
     }
 }
--- Assets/_Script/AI/FrameVulnerable.cs
+++ Assets/_Script/AI/FrameVulnerable.cs
@@ -4,23 +4,32 @@ using UnityEngine;
 
 public class FrameVulnerable : Frame
 {
-    public FrameStats stats;
     public override void HandleImpact(Frame other)
     {
         Vector3 vec1 = GetComponentInParent<Rigidbody>().velocity;
         Vector3 vec2 = other.GetComponentInParent<Rigidbody>().velocity;
+        float dot = Vector3.Dot(vec1.normalized, vec2.normalized);
+        dot *= -1; // reverse values so that a more negative dot product results in a higher damage value
+        dot += 1; // make all possible dot values positive
+        dot /= 2; // clamp from 0 to 1
         Frame frame2;
         if (other.GetType() == typeof(FrameArmored))
         {
             frame2 = other as FrameArmored;
+            float damageToThis = ImpactController.Instance.ArmoredDamageBase * dot * ImpactController.Instance.DamageMultiplier * vec2.magnitude;
+
         }
         else if (other.GetType() == typeof(FrameRegular))
         {
             frame2 = other as FrameRegular;
+            float damageToThis = ImpactController.Instance.ArmoredDamageBase * dot * ImpactController.Instance.DamageMultiplier * vec2.magnitude;
+
         }
         else if (other.GetType() == typeof(FrameVulnerable))
         {
             frame2 = other as FrameVulnerable;
+            float damageToThis = ImpactController.Instance.ArmoredDamageBase * dot * ImpactController.Instance.DamageMultiplier * vec2.magnitude;
+
         }
     }
 }
